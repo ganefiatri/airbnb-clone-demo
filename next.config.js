cc7dc0b47086e24acb050eb246360def29838639
ganefiatri <gicerf@gmail.com>
2022-10-26 03:41:30 +0700

    add search and mapbox

--- next.config.js
+++ next.config.js
@@ -3,5 +3,8 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['links.papareact.com']
+  },
+  env: {
+    mapbox_key: 'pk.eyJ1IjoiZ2FuZWZpYXRyaSIsImEiOiJjbDlvZnJ6ZjYwZWFnM3ZvOGQyNHVydmg0In0.f4ZvBVvt9GfkO0vE2FJ-Bw'
   }
 }
